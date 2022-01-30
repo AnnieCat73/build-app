1af34bc6ab1bd181aa9af96480c8e2fb810445c3
Annie <devannie777@gmail.com>
2022-01-30 06:05:57 +0000

    Star wars pseudo code set up

--- src/StarWars.js
+++ src/StarWars.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
 class StarWars extends React.Component {
+    getNewCharacter() {
+        console.log("Get new character!");
+    }
+    /*getNewCharacter = () => {
+
+    }*/
     render() {
         return (
-            <h1>This is star wars</h1>
+            <div>
+                <h1>Name</h1>
+                <p>Height cm</p>
+                <p>Homeworld: URL</p>
+                <ul>
+                    <li>Films in here</li>
+                </ul>
+                <button
+                    type="button"
+                    onClick={() => this.getNewCharacter()}
+                    className="btn">Randomise Character
+                </button>
+                {/*<button
+                    type="button"
+                    onClick={this.getNewCharacter}
+                    className="btn">Randomise Character
+                </button>*/}
+            </div>
         );
     }
 }
