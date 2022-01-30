8d70dfea7e3ded219bcee5c20f7cb6b568df0ad4
Annie <devannie777@gmail.com>
2022-01-30 05:32:33 +0000

    Click events

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import './App.css';
-import Item from './MyItem'
-import MyFunction from './MyFunction';
 
-const hello = <p>We are also saying hellosss</p>
+import StarWars from './StarWars.js';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      {hello}
-      <Item name="Gully" />
-      <Item name="Rhubarb" />
-      <MyFunction
-        name="MyFunction"
-        price="35"
-        make="Sanyo"
-      />
-      <Item name="Nozzybear" />
+      <header className="App-header">
+        <StarWars />
+      </header>
+
     </div>
   );
 }
---
+++ src/StarWars.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class StarWars extends React.Component {
+    render() {
+        return (
+            <h1>This is star wars</h1>
+        );
+    }
+}
+
+export default StarWars;
