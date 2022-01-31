9dd919225e8b28dbbc26c1c19d8227c1431dd5f3
Annie <devannie777@gmail.com>
2022-01-31 05:35:06 +0000

    Link and random characters added to Star Wars

--- src/StarWars.js
+++ src/StarWars.js
@@ -12,7 +12,8 @@ class StarWars extends React.Component {
         }
     }
     getNewCharacter() {
-        const url = "https://swapi.dev/api/people/1/"
+        const randomNumber = Math.round(Math.random() * 82)//SO go to swapi.dev/api/people - in browser and  enter you will get 82
+        const url = `https://swapi.dev/api/people/${randomNumber}/`//so can get a random charac of 82
         fetch(url)
             .then(response => response.json())
             .then(data => {
@@ -44,7 +45,7 @@ class StarWars extends React.Component {
                     <div>
                         <h1>{this.state.name}</h1>
                         <p>{this.state.height}</p>
-                        <p>Homeworld: {this.state.homeworld}</p>
+                        <p><a href={this.state.homeworld}>Homeworld</a></p>
                         <ul>
                             <li>{this.state.films}</li>
                         </ul>
