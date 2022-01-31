fa36b7c5a5d81536c8e3a100bb4ced1580ceea80
Annie <devannie777@gmail.com>
2022-01-31 05:13:04 +0000

    Star Wars button state generator added

--- src/StarWars.js
+++ src/StarWars.js
@@ -1,21 +1,55 @@
 import React from 'react';
 
 class StarWars extends React.Component {
-    getNewCharacter() {
-        console.log("Get new character!");
+    constructor() {//Initial state
+        super()
+        this.state = {
+            loadedCharacter: false, //false by default
+            name: null,
+            height: null,
+            homeworld: null,
+            films: []
+        }
     }
-    /*getNewCharacter = () => {
+    getNewCharacter() {
+        const url = "https://swapi.dev/api/people/1/"
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({//Updates initial state with new characters
+                    name: data.name,//as set it to data above and name in the object
+                    height: data.height,
+                    homeworld: data.homeworld,
+                    films: data.films,
+                    loadedCharacter: true  //so get the jsx below when true
+                })
+            })
+
+    }
+    /*
+    state = {
+        name: null,
+        height: null,
+        homeworld: null,
+        films: []
+    }
+    getNewCharacter = () => {
 
     }*/
     render() {
         return (
             <div>
-                <h1>Name</h1>
-                <p>Height cm</p>
-                <p>Homeworld: URL</p>
-                <ul>
-                    <li>Films in here</li>
-                </ul>
+                {//so only gets btn when refresh/AND so if state of 
+                    this.state.loadedCharacter &&//loaded char is true add div with this new state
+                    <div>
+                        <h1>{this.state.name}</h1>
+                        <p>{this.state.height}</p>
+                        <p>Homeworld: {this.state.homeworld}</p>
+                        <ul>
+                            <li>{this.state.films}</li>
+                        </ul>
+                    </div>
+                }
                 <button
                     type="button"
                     onClick={() => this.getNewCharacter()}
