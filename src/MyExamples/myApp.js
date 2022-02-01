b6a7010a0d3b88557b20cbcd5251ebb305cf4119
Annie <devannie777@gmail.com>
2022-02-01 05:25:10 +0000

    My Star Wars up set up

---
+++ src/MyExamples/myApp.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import StarWars from './StarWars.js';
+
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <StarWars />
+            </header>
+
+        </div>
+    );
+}
+export default App;
