80afc7204724ec527715643b33450cd03f05a1a3
Annie <devannie777@gmail.com>
2022-02-02 05:14:50 +0000

    Star Wars API added

--- src/StarWars.js
+++ src/StarWars.js
@@ -1,29 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import FilmItemRow from './FilmItemRow';
 
-state = {
-    name: null,
-    height: null,
-    homeworld: null,
-    films: []
-}
 
-const url = `https://swapi.dev/api/people/`;
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
 
-    })
 
 class StarWars extends React.Component {
+
+    state = {
+        name: null,
+        height: null,
+        homeworld: null,
+        films: []
+    }
+
+    /*const url = `https://swapi.dev/api/people/`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+        this.setState({
+            name: data.name,
+            height: data.height,
+            homeworld: data.homeworld,
+            films: data.films
+        })
+    })*/
+
     render() {
         const movies = this.state.films.map(film =>
             <FilmItemRow url={film} key={film} />);
         return (
             <div>
                 <div className="info">
-                    <h1>Name</h1>
-                    <p>height</p>
+                    <h1>{this.state.name}</h1>
+                    <p>{this.state.height}</p>
+                    <p><a href={this.state.homeworld}>Homeworld</a></p>
                     <ul className="list">
                         {movies}
                     </ul>
