63647c87e08780cb3758429c4171b2492f5e407f
Annie <devannie777@gmail.com>
2022-02-02 05:50:53 +0000

    Star Wars part 2

--- src/StarWars.js
+++ src/StarWars.js
@@ -13,33 +13,43 @@ class StarWars extends React.Component {
         films: []
     }
 
-    /*const url = `https://swapi.dev/api/people/`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-        this.setState({
-            name: data.name,
-            height: data.height,
-            homeworld: data.homeworld,
-            films: data.films
-        })
-    })*/
+    getRandomCharacter = () => {
+        const randomCharacter = Math.round(Math.random() * 82);
+        const url = `https://swapi.dev/api/people/${randomCharacter}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    name: data.name,
+                    height: data.height,
+                    homeworld: data.homeworld,
+                    films: data.films,
+                    loadedCharacter: true
+                })
+            })
+    }
+
 
     render() {
-        const movies = this.state.films.map(film =>
-            <FilmItemRow url={film} key={film} />);
+        const movies = this.state.films.map((film, i) => {
+            return <FilmItemRow key={i} url={film} />
+        });
         return (
             <div>
-                <div className="info">
-                    <h1>{this.state.name}</h1>
-                    <p>{this.state.height}</p>
-                    <p><a href={this.state.homeworld}>Homeworld</a></p>
-                    <ul className="list">
-                        {movies}
-                    </ul>
-                </div>
+                {
+                    this.state.loadedCharacter &&
+                    <div className="info">
+                        <h1>{this.state.name}</h1>
+                        <p>Height: {this.state.height} cm</p>
+                        <p><a href={this.state.homeworld}>Homeworld</a></p>
+                        <ul className="list">
+                            {movies}
+                        </ul>
+                    </div>
+                }
 
-                <button className="btn">Get Character</button>
+
+                <button className="btn" onClick={this.getRandomCharacter}>Get Character</button>
             </div>
         );
     }
