5b37b9c38d51a5813f00fab87894de5bf24add9d
Annie <devannie777@gmail.com>
2022-02-03 05:24:44 +0000

    Star Wars2 set up

--- src/MyExamples/myStarWars.js
+++ src/MyExamples/myStarWars.js
@@ -71,3 +71,62 @@ class StarWars extends React.Component {
 }
 
 export default StarWars;
+
+/*OR WITH SKILLCRUSH TAUGHT STATE:
+
+import React from 'react';
+import FilmItemRow from './FilmItemRow';
+
+class StarWars extends React.Component {
+
+    state = {
+        name: null,
+        height: null,
+        homeworld: null,
+        films: []
+    }
+
+    getRandomCharacter = () => {
+        const randomCharacter = Math.round(Math.random() * 82);
+        const url = `https://swapi.dev/api/people/${randomCharacter}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    name: data.name,
+                    height: data.height,
+                    homeworld: data.homeworld,
+                    films: data.films,
+                    loadedCharacter: true
+                })
+            })
+    }
+
+
+    render() {
+        const movies = this.state.films.map((film, i) => {
+            return <FilmItemRow key={i} url={film} />
+        });
+        return (
+            <div>
+                {
+                    this.state.loadedCharacter &&
+                    <div className="info">
+                        <h1>{this.state.name}</h1>
+                        <p>Height: {this.state.height} cm</p>
+                        <p><a href={this.state.homeworld}>Homeworld</a></p>
+                        <ul className="list">
+                            {movies}
+                        </ul>
+                    </div>
+                }
+
+
+                <button className="btn" onClick={this.getRandomCharacter}>Get Character</button>
+            </div>
+        );
+    }
+}
+
+export default StarWars;
+*/
--- src/StarWars.js
+++ src/StarWars.js
@@ -1,55 +1,15 @@
 import React from 'react';
-import FilmItemRow from './FilmItemRow';
-
-
-
 
 class StarWars extends React.Component {
 
-    state = {
-        name: null,
-        height: null,
-        homeworld: null,
-        films: []
-    }
 
-    getRandomCharacter = () => {
-        const randomCharacter = Math.round(Math.random() * 82);
-        const url = `https://swapi.dev/api/people/${randomCharacter}`;
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                this.setState({
-                    name: data.name,
-                    height: data.height,
-                    homeworld: data.homeworld,
-                    films: data.films,
-                    loadedCharacter: true
-                })
-            })
-    }
 
 
     render() {
-        const movies = this.state.films.map((film, i) => {
-            return <FilmItemRow key={i} url={film} />
-        });
+
         return (
             <div>
-                {
-                    this.state.loadedCharacter &&
-                    <div className="info">
-                        <h1>{this.state.name}</h1>
-                        <p>Height: {this.state.height} cm</p>
-                        <p><a href={this.state.homeworld}>Homeworld</a></p>
-                        <ul className="list">
-                            {movies}
-                        </ul>
-                    </div>
-                }
 
-
-                <button className="btn" onClick={this.getRandomCharacter}>Get Character</button>
             </div>
         );
     }
