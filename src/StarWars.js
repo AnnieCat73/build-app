577b4193d7be28e09cff4d4328d1bbf56486252d
Annie <devannie777@gmail.com>
2022-02-04 06:20:27 +0000

    Star Wars State added

--- src/StarWars.js
+++ src/StarWars.js
@@ -8,22 +8,35 @@ class StarWars extends React.Component {
         affiliations: []
     }
     getNewCharacter() {
-        const randomNumber = Math.round(Math.random() * 88);
-        const url = ``
+        const randomCharacter = Math.round(Math.random() * 88);
+        const url = `https://github.com/akabab/starwars-api/tree/master/api/id/${randomCharacter}`;
+        console.log(url);
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    name: data.name,
+                    height: data.height,
+                    homeworld: data.homeworld,
+                    affiliations: data.affiliations
+                })
+            })
 
     }
 
 
 
     render() {
-
+        const associates = this.state.affiliations.map((affiliate, i) => {
+            return <p>key={i} affiliate={affiliate}</p>
+        })
         return (
             <div>
                 <div>
                     <h1>Name: {this.state.name}</h1>
                     <p>Height: {this.state.height} cm</p>
                     <p>Homeworld: {this.state.homeworld}</p>
-                    <p>Affiliations: {this.state.affiliations}</p>
+                    <p>Affiliations: {associates}</p>
                 </div>
                 <button className="btn" onClick={this.getNewCharacter}>Get Character</button>
             </div>
