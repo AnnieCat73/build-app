0627ce5b26dcd64e34d0c7e4fd2b79ee4223ef00
Annie <devannie777@gmail.com>
2022-03-11 06:23:14 +0000

    New Star Wars jsx added

--- src/NewStarWars.js
+++ src/NewStarWars.js
@@ -1,21 +1,27 @@
 import React from 'react'
+import { useState } from 'react';
 
 function NewStarWars() {
+
+  //const [loadedCharacter, setLoadedCharacter] = useState(true);
   return (
-    <div className="main">
-      <img className="image" src={'https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg'}></img>
-      <div className="info">
-        <h3>Name: </h3>
-        <p>Height: </p>
-        <p>Homeworld: </p>
-        <p>Species: </p>
-        <ul>Affiliations:</ul>
-        <ul>Masters:</ul>
-        <ul>Apprentices:</ul>
+    <>
+      <div className="main">
+        <img className="image" src={'https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg'}></img>
+        <div className="text-content">
+          <div className="info">
+            <h3>Name: </h3>
+            <p>Height: </p>
+            <p>Homeworld: </p>
+            <p>Species: </p>
+            <ul>Affiliations:</ul>
+            <ul>Masters:</ul>
+            <ul>Apprentices:</ul>
+          </div>
+          <button className="btn">Get Character</button>
+        </div>
       </div>
-      
-      
-    </div>
+    </>
   )
 }
 
