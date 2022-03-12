c3d4891278a80721d1fb4f2a77d69db187a40e7c
Annie <devannie777@gmail.com>
2022-03-12 05:59:49 +0000

    New Star Wars app state component set up part 2

--- src/MiaStarWars.js
+++ src/MiaStarWars.js
@@ -3,6 +3,7 @@ import React from 'react';
 class MiaStarWars extends React.Component {
 
   state = {
+    //loadedCharacter: false,
     image: null,
     name: null,
     height: null,
@@ -17,10 +18,38 @@ class MiaStarWars extends React.Component {
   render() {
 
     return (
-      <div>
+      <>
+        <div className="main">
+          
+          <div>
+            <img className="image" src={'https://vignette.wikia.nocookie.net/starwars/images/b/b8/Dooku_Headshot.jpg'}/> 
+            <div>
+              <h1>Name: {this.state.name}</h1>
+              <p>Height: </p>
+              <p>Mass: </p>
+              <p>Eye Colour: </p>
+              <ul>Homeworld: </ul>
+              <ul>Affiliations: </ul>
+              <ul>Apprentices: </ul>
+              <ul>Masters: </ul>
+            </div>
+              
+
+          
+          </div>
+           
+         
+          <button
+          className="btn" 
+          type="button"
+          >Get Character
+
+          </button>
 
       </div>
 
+      </>
+      
     );
   }
 }
