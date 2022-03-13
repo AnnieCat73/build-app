00653f9dbbac32f8f4a4ccb3d96433dadbfedcc4
Annie <devannie777@gmail.com>
2022-03-13 05:23:42 +0000

    JSX set up

--- src/MiaStarWars.js
+++ src/MiaStarWars.js
@@ -15,36 +15,44 @@ class MiaStarWars extends React.Component {
     masters: []
   }
 
+  getRandomCharacter = () => {
+        
+  }
+
   render() {
 
     return (
+      
       <>
         <div className="main">
           
-          <div>
+          <div className="content">
             <img className="image" src={'https://vignette.wikia.nocookie.net/starwars/images/b/b8/Dooku_Headshot.jpg'}/> 
             <div>
-              <h1>Name: {this.state.name}</h1>
-              <p>Height: </p>
-              <p>Mass: </p>
-              <p>Eye Colour: </p>
-              <ul>Homeworld: </ul>
-              <ul>Affiliations: </ul>
-              <ul>Apprentices: </ul>
-              <ul>Masters: </ul>
+              <div className="text-content">
+                <h1>Name: {this.state.name}</h1>
+                <p>Height: </p>
+                <p>Mass: </p>
+                <p>Eye Colour: </p>
+                <ul>Homeworld: </ul>
+                <ul>Affiliations: </ul>
+                <ul>Apprentices: </ul>
+                <ul>Masters: </ul>
+              </div>
+
+              <button
+              className="btn" 
+              type="button"
+              onClick={this.getRandomCharacter}>Get Character</button>
             </div>
+            
               
 
           
           </div>
            
          
-          <button
-          className="btn" 
-          type="button"
-          >Get Character
-
-          </button>
+          
 
       </div>
 
